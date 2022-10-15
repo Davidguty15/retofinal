978ef314e92b25ad3a9d4bc71101dea7ff65c2d5
CTelloMeli <carlos.telloruiz@mercadolibre.com.co>
2022-10-14 20:08:40 -0500

    test

--- src/main/java/com/example/retos345/entities/ReportClient.java
+++ src/main/java/com/example/retos345/entities/ReportClient.java
@@ -1,11 +1,32 @@
 package com.example.retos345.entities;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Sortbyroll implements Comparator<Reservation>
+{
+    // Used for sorting in ascending order of
+    // roll number
+    public int compare(Reservation a, Reservation b)
+    {
+        return a.getIdReservation() - b.getIdReservation();
+    }
+}
+
 public class ReportClient{
     private int total;
     private Client client;
 
     public ReportClient(Client client){
         this.client = client;
+        List<Reservation> arr = new ArrayList<>(client.getReservations());
+        Collections.sort(arr, new Sortbyroll());
+        Set<Reservation> hSet = new HashSet<Reservation>(arr);
+        this.client.setReservations(hSet);
         this.total = client.getReservations().size();
     }
 
